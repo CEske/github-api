d0466f7746d7532322474a126fb74366e2a761b6
Eske <christian.eske@yahoo.com>
2022-12-04 15:24:06 +0100

    Cleaned

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.routes = void 0;
-const express_1 = require("express");
-exports.routes = (0, express_1.Router)();
-// Importer alle routes
-const newCommit_1 = require("./newCommit");
-exports.routes.use(newCommit_1.newCommit);
